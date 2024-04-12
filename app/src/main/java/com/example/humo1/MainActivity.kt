efe0380442aacfa1ff7d8aca485874fbb3fac280
DamirIsRegularGuy <damir4ik2004@gmail.com>
2024-04-12 20:45:52 +0500

    password

--- app/src/main/java/com/example/humo1/MainActivity.kt
+++ app/src/main/java/com/example/humo1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.humo1
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
@@ -13,11 +16,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.switchmaterial.SwitchMaterial
+import second.MainActivity2
 
 class MainActivity : AppCompatActivity() {
     private var editTextView: EditText? = null
+    private var editTextView1: EditText? = null
     private var textView: TextView? = null
     private var button: Button? = null
+    private var button1: Button? = null
     private var checkBox: CheckBox? = null
     private var switcher: SwitchMaterial? = null
     private var radbutton: RadioButton? = null
@@ -25,11 +31,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.vie3)
-
+        setContentView(R.layout.activity_main)
+        /*
         editTextView = findViewById<EditText>(R.id.text_edit)
         textView = findViewById<TextView>(R.id.text_view)
         button = findViewById<Button>(R.id.first_button)
+        button1 = findViewById<Button>(R.id.second_button)
         checkBox = findViewById<CheckBox>(R.id.checkbox)
         switcher = findViewById<SwitchMaterial>(R.id.switch_mat)
         colorgroup = findViewById<RadioGroup>(R.id.color)
@@ -83,6 +90,57 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+      */
+
+        val button1 = findViewById<Button>(R.id.second_button)
+        val editTextView = findViewById<EditText>(R.id.edit_tex)
+        val editTextView1 = findViewById<EditText>(R.id.edit_text)
+
+        button1.isEnabled = false
+
+        fun updateButtonState() {
+            val text1 = editTextView.text?.toString()?.trim()
+            val text2 = editTextView1.text?.toString()?.trim()
+
+            button1.isEnabled = !text1.isNullOrEmpty() && !text2.isNullOrEmpty() && text1 == text2
+
+        }
+
+        editTextView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                updateButtonState()
+            }
+
+        })
+
+        editTextView1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                updateButtonState()
+            }
+
+        })
+
+        button1?.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
+
+
 
 
     }
---
+++ app/src/main/java/second/MainActivity2.kt
@@ -0,0 +1,16 @@
+package second
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.humo1.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_second)
+
+        }
+    }
