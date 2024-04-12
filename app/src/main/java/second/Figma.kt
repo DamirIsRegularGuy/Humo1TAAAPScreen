defe96cf1a39d642648f4b7b797440ad46438fd8
DamirIsRegularGuy <damir4ik2004@gmail.com>
2024-04-12 20:45:52 +0500

    password

---
+++ app/src/main/java/second/Figma.kt
@@ -0,0 +1,12 @@
+package second
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.humo1.R
+
+class Figma: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firma1)
+    }
+}
